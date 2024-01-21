df4e3a7d57c0404b32d844d288ffdf0a2e260a65
ElenaN <maggika@mail.ru>
2024-01-21 12:54:12 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author firstAuthor = new Author("Joanne", "Rowling");
+        Author secondAuthor = new Author("Ernest", "Hemingway");
+
+        Book firstBook = new Book("Harry Potter and The Half-Blood Prince", firstAuthor, 2005);
+        Book secondBook = new Book("A Moveable Feast", secondAuthor, 2000);
+
+        secondBook.setPublicationYear(1964);
+
+        System.out.println(firstBook.getAuthor().getFirstName() + " " + firstBook.getAuthor().getLastName() + ", " + firstBook.getTitle() + ", " + firstBook.getPublicationYear());
+        System.out.println(secondBook.getAuthor().getFirstName() + " " + secondBook.getAuthor().getLastName() + ", " + secondBook.getTitle() + ", " + secondBook.getPublicationYear());
     }
 }
